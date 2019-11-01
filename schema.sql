221dbc697a3864fb80395bccb984d43f6028a467
Eric Fuller <efuller82@gmail.com>
2019-11-01 17:05:27 -0500

    first html route setup; server setup

---
+++ schema.sql
@@ -0,0 +1,4 @@
+-- Drops the 'blog' if it exists currently 
+DROP DATABASE IF EXISTS blog;
+-- Creates the 'blog' database
+CREATE DATABASE blog;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// !don't have models made yet
+// const db = require('./models');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+// ! don't have routes made yet
+// require('./routes/api-routes')(app);
+require('./routes/html-routes')(app);
+
+// ! don't have a db yet
+// db.sequelize.sync({ force: true }).then(function () {
+app.listen(PORT, function () {
+    console.log('App is listening on PORT ' + PORT);
+});
+// });
