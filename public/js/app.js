77612e1f96c057b9c9b71ea05e3ccf005f1e7020
Eric Fuller <efuller82@gmail.com>
2019-11-01 20:52:45 -0500

    starting over again

--- public/js/app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $(document).on('click', '#post-to-blog', makePost);
-
-    function makePost() {
-        alert('click registered');
-    }
-})
--- schema.sql
+++
@@ -1,4 +0,0 @@
--- Drops the 'blog' if it exists currently 
-DROP DATABASE IF EXISTS blog;
--- Creates the 'blog' database
-CREATE DATABASE blog;
